c208e881498b743b875832d72c39abfeec99b1ed
alby.giaco <alby.giaco@live.it>
2023-07-23 00:53:12 +0200

    use slap delete

--- trust-explorer/src/pages/index.tsx
+++ trust-explorer/src/pages/index.tsx
@@ -2,18 +2,9 @@ import Homepage from "../component/Homepage";
 import MainSection from "../component/MainSection";
 import Hero from "../component/Hero";
 import { Container } from "@chakra-ui/react";
-import { useSnap } from "../hooks/useSnap";
 import { useAsyncMemo } from "use-async-memo";
 
 export default function Home() {
-  const { detectSnap, connectSnap, installSnap } = useSnap();
-
-  const detect = useAsyncMemo(async () => {
-    await installSnap();
-    await detectSnap();
-    await connectSnap();
-  }, []);
-
   return (
     <Container maxW={"6xl"}>
       <Homepage>
